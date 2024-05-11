ba8781ac987a580cf8ebc90379b6fd01fc4a17a5
Yuval Confino <yuvalconfino11@gmail.com>
2024-05-11 14:07:37 +0300

    added almost finished get color

--- hw3.py
+++ hw3.py
@@ -17,15 +17,55 @@ def render_scene(camera, ambient, lights, objects, screen_size, max_depth):
             ray = Ray(origin, direction)
 
             color = np.zeros(3)
+            min_distance, nearest_object = ray.nearest_intersected_object(objects)
+            get_color(ray, nearest_object, ambient, lights, intersection_point)
 
             # This is the main loop where each pixel color is computed.
-            # TODO
+
 
             
             # We clip the values between 0 and 1 so all pixel values will make sense.
             image[i, j] = np.clip(color,0,1)
 
     return image
+
+
+def get_color(ray, nearest_object, ambient, lights, p, level):
+    global_ambient = ambient
+    material_ambient = nearest_object.ambient
+    color = global_ambient * material_ambient
+    for i in range(len(lights)):
+        light = lights[i]
+        light_shadowed = return_if_light_shadow(light, ray, p ,nearest_object)
+        if light_shadowed == 1:
+            color += calc_diffuse_color(ray ,light, nearest_object) + calc_specular_color(ray,light, nearest_object)
+
+    level -= 1
+    if level == 0:
+        return color
+    else:
+
+def calc_diffuse_color(ray, light, nearest_object):
+    material_diffuse = nearest_object.diffuse
+    intersection_normal =
+    intersection_to_light =
+    light_intensity = light.get_intensity(nearest_object)
+    return material_diffuse * light_intensity * (np.dot( intersection_normal,intersection_to_light))
+
+def calc_specular_color(ray, light, nearest_object):
+    material_specular = nearest_object.specular
+    intersection_to_eye =
+    intersection_to_reflected_light =
+    light_intensity = light.get_intensity(nearest_object)
+    return  material_specular * light_intensity * ((np.dot(intersection_to_eye,intesection_to_reflected_light))**nearest_object.shininess)
+
+
+def return_if_light_shadow(light, ray, p ,nearest_object):
+    light_ray = light.get_light_ray(p)
+
+
+
+
 
 
 # Write your own objects and lights
