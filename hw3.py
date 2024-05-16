2bde76b370da7cf58c5a0b4e9555adb64cd08cec
Yuval Confino <yuvalconfino11@gmail.com>
2024-05-16 17:27:27 +0300

    changed plane itersect and q6

--- hw3.py
+++ hw3.py
@@ -122,35 +122,105 @@ def construct_reflective_ray(ray, p, object):
 # Write your own objects and lights
 # done by us
 def your_own_scene():
-    sphere_a = Sphere(center=[0.5, 0.4, -1.5], radius=0.5)
-    sphere_a.set_material(ambient=[0.5, 0.5, 1], diffuse=[0.5, 0.5, 1], specular=[0.3, 0.3, 0.3], shininess=150, reflection=0.7)
+    objects = []
+    lights = []
 
-    sphere_b = Sphere(center=[-0.5, -0.4, -1], radius=0.3)
-    sphere_b.set_material(ambient=[1, 0, 0], diffuse=[1, 0, 0], specular=[0.3, 0.3, 0.3], shininess=130, reflection=0.5)
+    # Pikachu's Body
+    pikachu_body = Sphere(center=[0, 0, -5], radius=1.7)
+    pikachu_body.set_material(ambient=np.array([1, 1, 0]),  # Yellow ambient
+                              diffuse=np.array([1, 1, 0]),  # Yellow diffuse
+                              specular=np.array([1, 1, 0]),  # Yellow specular
+                              shininess=50,
+                              reflection=np.array([0.5, 0.5, 0.5]))
+    objects.append(pikachu_body)
+    # Pikachu's Eyes
+    pikachu_eye_left = Sphere(center=[-0.6, 0.75, -3.5], radius=0.25)  # Moved closer to the front
+    pikachu_eye_left.set_material(ambient=np.array([0, 0, 0]),
+                                  diffuse=np.array([0, 0, 0]),
+                                  specular=np.array([1, 1, 1]),
+                                  shininess=100,
+                                  reflection=np.array([0.5, 0.5, 0.5]))
+    objects.append(pikachu_eye_left)
 
-    vertices_pyramid = [
-        [-1.5, -0.75, -2],  
-        [-1.5, -2, -2],     
-        [0.5, -2, -2],      
-        [0.5, -0.75, -2],   
-        [-0.5, -2, -3]      
-    ]
-    pyramid = Pyramid(vertices_pyramid)
-    pyramid.set_material([1, 0.5, 0], [1, 0.5, 0], [0.2, 0.2, 0.2], 120, 0.6)
-    pyramid.apply_materials_to_triangles()
+    pikachu_eye_right = Sphere(center=[0.6, 0.75, -3.5], radius=0.25)  # Moved closer to the front
+    pikachu_eye_right.set_material(ambient=np.array([0, 0, 0]),
+                                   diffuse=np.array([0, 0, 0]),
+                                   specular=np.array([1, 1, 1]),
+                                   shininess=100,
+                                   reflection=np.array([0.5, 0.5, 0.5]))
+    objects.append(pikachu_eye_right)
 
-    plane = Plane(normal=[0, 1, 0], point=[0, -1, 0])
-    plane.set_material(ambient=[0.2, 0.2, 0.2], diffuse=[0.2, 0.2, 0.2], specular=[1, 1, 1], shininess=1000, reflection=0.5)
+    # Pikachu's Ears
+    # Left Ear
+    vertices_ear_left = np.array([
+        [-1.5, 3, -5],
+        [-1.9, 2.2, -5],
+        [-1.1, 2.2, -5],
+        [-1.5, 2.5, -4.7],
+        [-1.5, 2.5, -5.3]
+    ])
+    pikachu_ear_left = Pyramid(vertices_ear_left)
+    pikachu_ear_left.set_material([0, 0, 0], [0, 0, 0], [0.3, 0.3, 0.3], 50, 0.5)
+    pikachu_ear_left.apply_materials_to_triangles()
+    objects.append(pikachu_ear_left)
 
-    # Lights
-    light1 = PointLight(intensity=[0.8, 0.8, 0.8], position=[2, 2, 0], kc=0.1, kl=0.05, kq=0.01)
-    light2 = PointLight(intensity=[0.8, 0.8, 0.8], position=[-2, 2, 0], kc=0.1, kl=0.05, kq=0.01)
+    # Pikachu's Right Ear as a Pyramid
+    vertices_ear_right = np.array([
+        [1.5, 3, -5],
+        [1.9, 2.2, -5],
+        [1.1, 2.2, -5],
+        [1.5, 2.5, -4.7],
+        [1.5, 2.5, -5.3]
+    ])
+    pikachu_ear_right = Pyramid(vertices_ear_right)
+    pikachu_ear_right.set_material([0, 0, 0], [0, 0, 0], [0.3, 0.3, 0.3], 50, 0.5)
+    pikachu_ear_right.apply_materials_to_triangles()
+    objects.append(pikachu_ear_right)
 
-    # Environment
-    camera = [0, 0, 5]  # Adjust camera position for a better view
+    # Pokeball
+    pokeball_bottom = Sphere(center=[3, -1, -3], radius=0.5)
+    pokeball_bottom.set_material(ambient=np.array([1, 1, 1]),
+                                 diffuse=np.array([1, 1, 1]),
+                                 specular=np.array([1, 1, 1]),
+                                 shininess=300,
+                                 reflection=np.array([0.5, 0.5, 0.5]))
+    objects.append(pokeball_bottom)
 
-    # Scene
-    objects = [sphere_a, sphere_b, pyramid, plane]
-    lights = [light1, light2]
+    pokeball_top = Sphere(center=[3, -0.5, -3], radius=0.5)
+    pokeball_top.set_material(ambient=np.array([1, 0, 0]),
+                              diffuse=np.array([1, 0, 0]),
+                              specular=np.array([1, 0, 0]),
+                              shininess=300,
+                              reflection=np.array([0.5, 0.5, 0.5]))
+    objects.append(pokeball_top)
+    # Ground plane
+    plane = Plane(normal=[0, 1, 0], point=[0, -2.5, 0])
+    plane.set_material(ambient=np.array([1, 1, 1]),  # White ambient
+                       diffuse=np.array([1, 1, 1]),  # White diffuse
+                       specular=np.array([0.5, 0.5, 0.5]),  # Mild specular to avoid too much shine
+                       shininess=10,  # Lower shininess for a less glossy surface
+                       reflection=np.array([0.1, 0.1, 0.1]))
+    objects.append(plane)
+
+    # Background plane - setting this to white
+    background = Plane(normal=[0, 0, 1], point=[0, 0, -30])
+    background.set_material(ambient=np.array([1, 1, 1]),  # White ambient
+                            diffuse=np.array([1, 1, 1]),  # White diffuse
+                            specular=np.array([0.5, 0.5, 0.5]),  # Mild specular
+                            shininess=20,  # Moderate shininess to reflect some light
+                            reflection=np.array([0.1, 0.1, 0.1]))
+    objects.append(background)
+
+    # Lighting setup
+    directional_light = DirectionalLight(intensity=np.array([1.0, 1.0, 1.0]), direction=[1, -1, -1])
+    point_light = PointLight(intensity=np.array([0.8, 0.8, 0.8]), position=[2, 2, -3], kc=1, kl=0.1, kq=0.01)
+    lights.append(directional_light)
+    lights.append(point_light)
+
+
+
+    # Camera setup
+    camera = np.array([0, 0, 1])
 
     return camera, lights, objects
+
